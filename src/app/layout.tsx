5bed9fc2b0ad33566cb72ddccef8ca22a860f19b
gamemanuel <themathnight@gmail.com>
2025-11-01 12:55:14 -0700

    updated meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,26 @@ const inter = Inter({
     variable: "--font-sans", // This CSS variable is used by shadcn/ui
 });
 
+
 export const metadata: Metadata = {
-    title: "My Glassmorphism Site",
-    description: "A site with shadcn/ui and glassmorphism",
+    title: "CameronThrap.com",
+    description: "Respectably, caring, and efficiently creating art and entertainment for others to enjoy",
+    openGraph: {
+        title: "CameronThrap.com",
+        description: "Respectably, caring, and efficiently creating art and entertainment for others to enjoy",
+        url: 'https://cameronthrap.rrappleye.com',
+        siteName: 'CameronThrap.com',
+        images: [
+            {
+                url: '/src/ContactImage.jpg', // Must be an absolute URL for social media to read correctly, which Next.js handles with metadataBase
+                width: 1200,
+                height: 630,
+                alt: 'Cameron Thrap Website Banner Image',
+            },
+        ],
+        locale: 'en_US',
+        type: 'website',
+    },
 };
 
 export default function RootLayout({
